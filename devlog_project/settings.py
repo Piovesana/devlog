914a418ac089a0066d3101f298f5c00ddf30b0a9
gabriel.piovesana <gabriel.piovesana@autojun.com.br>
2026-06-19 15:08:03 -0300

    Configuracoes para o deploy

--- devlog_project/settings.py
+++ devlog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0eh0xm@w911#=$&1cio7@&n_^c(+b%@47m1=p)$9k4)gh(wd9*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
